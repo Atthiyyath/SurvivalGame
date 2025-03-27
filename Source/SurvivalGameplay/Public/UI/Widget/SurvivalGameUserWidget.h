81ea77cb95698a86e5afdfe94df3ba62b1ad823c
Atia <atthiyya@gmail.com>
2025-03-28 05:21:23 +0700

    init Project

---
+++ Source/SurvivalGameplay/Public/UI/Widget/SurvivalGameUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SurvivalGameUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALGAMEPLAY_API USurvivalGameUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SurvivalGameplay/SurvivalGameplayGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivalGameplayGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASurvivalGameplayGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASurvivalGameplayGameMode();
+};
+
+
+
